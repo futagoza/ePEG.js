69fdc35e01b66d4c27824e54c0ad8171ab3baa23
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2016-07-04 03:19:36 +0100

    moved parser generator
    Attempt No. 9 to fix 'No such file or directory' error from travis

---
+++ scripts/generate-parser.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/*--------- 1) Dependencies ---------*/
+
+var fs = require('fs')
+var peg = require('pegjs-dev')
+var mkdirp = require('mkdirp')
+
+/*--------- 2) Options ---------*/
+
+var argv = process.argv.slice(2)
+
+var options = {
+  format: 'bare',
+  optimize: ~argv.indexOf('--size') ? 'size' : 'speed',
+  output: "source",
+  trace: argv.indexOf('--trace') !== -1
+}
+
+/*--------- 3) Generate parser ---------*/
+
+mkdirp.sync('lib')
+
+var source = fs.readFileSync('src/parser.pegjs', 'utf-8')
+
+var output = '\'use strict\';\n\n'
+output += 'module.exports = '
+output += peg.generate(source, options)
+output += '\n'
+
+fs.writeFileSync('lib/parser.js', output)
