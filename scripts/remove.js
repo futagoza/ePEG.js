cd7f6342041571ab98992b669ab6311f3540a509
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2016-07-04 21:06:22 +0100

    conditonally remove and display info
    Before 'scripts/remove.js' would just try and remove files and
    directories and then would display 'Removed ...' message, without
    checking if the content exists. This fix's that :)

--- scripts/remove.js
+++ scripts/remove.js
@@ -1,15 +1,26 @@
 'use strict'
 
+var fs = require('fs')
 var rimraf = require('rimraf')
 var argv = process.argv.slice(2)
 
+function handleError ( err ) {
+  if ( err ) {
+    console.error(err.stack || err.message || err)
+    process.exit(1)
+  }
+}
+
 function remove ( path ) {
-  rimraf(path, function(err){
+  fs.lstat(path, function(err){
     if ( err ) {
-      console.error(err.stack || err.message || err)
-      process.exit(1)
+      if (err.code != 'ENOENT') handleError(err)
+    } else {
+      rimraf(path, function(err){
+        handleError(err)
+        console.log('Removed ' + path)
+      })
     }
-    console.log('Removed ' + path)
   })
 }
 
