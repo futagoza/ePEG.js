c42585de1184055d1c3f00415550844cdf13678d
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2016-07-10 01:01:06 +0100

    implemented working parser generator
    implemented 'bin/epeg.js', an upgraded version of 'pegjs-dev/bin/pegjs'
    
    - added '-f, --format <mode>' option
    - added '--verbose' option
    - added support for multiple input files as 1 grammar
    - added support for glob'ed input files
    - ouputFile will be guessed even from a glob now
    
    impelmented 'lib/epeg.js', an upgraded version of 'pegjs-dev/lib/peg.js'
    
    - exports versions object, similar to 'process.versions'
    - exports built-in plugins
    - exports 'setDefaultPlugins' and 'restoreDefaultPlugins'
    - exports 'epeg.generate', the ePeg wrapper for 'peg.generate'

--- src/lib/epeg.js
+++ src/lib/epeg.js
@@ -1,3 +1,40 @@
 'use strict'
 
+import * as peg from 'pegjs-dev'
 
+export const VERSION = require('../package.json').version
+export const versions = {
+  'node': process.versions.node,
+  'epeg.js': VERSION,
+  'pegjs': peg.VERSION,
+  'pegjs-dev': require('pegjs-dev/package.json').version
+}
+
+export const GrammarError = peg.GrammarError
+export const parser = require('./parser')
+export const compiler = peg.compiler
+
+export const IncludePlugin = require('./plugins/include')
+export const ImportPlugin = require('./plugins/import')
+export const FilesOptionPlugin = require('./plugins/files-option')
+
+export var defaultPlugins = null
+
+export function setDefaultPlugins ( ...plugins ) {
+  defaultPlugins = plugins
+}
+
+export function restoreDefaultPlugins ( ) {
+  defaultPlugins = [IncludePlugin, ImportPlugin, FilesOptionPlugin]
+}
+restoreDefaultPlugins()
+
+export function generate ( grammar, options = {} ) {
+  let plugins = "plugins" in options ? options.plugins : []
+  if ( defaultPlugins && defaultPlugins.length ) {
+    options = Object.assign(options, {
+      plugins: defaultPlugins.concat(...plugins)
+    })
+  }
+  return peg.generate(grammar, options)
+}
