70212721c4e42a0cd0d226c297213de9b6af0873
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2016-08-20 16:02:10 +0100

    implemented include plugin
    This plugin adds a pass to the preprocess stage that parse's
    the 'dependencies' property in the grammer's AST, inlining
    any local '*.pegjs' or '*.js' files requeseted using the
    include ast added previosly for  '@include ...'

--- src/lib/plugins/include.js
+++ src/lib/plugins/include.js
@@ -1,13 +1,37 @@
 'use strict'
 
-const parser = require('../parser')
+import { inlineGrammer, extendInitializer } from '../utils'
+import { dirname, extname, join } from 'path'
+import { readFileSync } from 'fs'
 
 export function use ( config, options ) {
   if ( options.disableIncludes !== true ) {
 
-    config.parser = parser
+    if ( !config.passes.preprocess ) {
+      config.passes = Object.assign({ preprocess: [] }, config.passes)
+    }
 
-    // ...
+    config.passes.preprocess.push(( ast, options ) => {
+      const dependencies = ast.dependencies
+      if ( Array.isArray(dependencies) && dependencies.length ) {
+        ast.dependencies.forEach(dependency => {
+
+          if ( dependency.type === 'include' ) {
+            const ext = extname(dependency.path)
+            const root = dirname(dependency.location.filename)
+            var filename = join(root, dependency.path)
+
+            if ( ext === '.pegjs' ) {
+              inlineGrammer(filename, ast, config.parser)
+            }
+            else if ( ext === '.js' ) {
+              extendInitializer(ast, readFileSync(filename, 'utf-8'))
+            }
+          }
+
+        })
+      }
+    })
 
   } 
 }
