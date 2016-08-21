9fb6803e1271f5a210dde141330ebb4900339d6d
Futago-za Ryuu <futagoza.ryuu@gmail.com>
2016-08-21 19:17:03 +0100

    rearranged to optimise usage of 'options.files'

--- src/lib/plugins/files-option.js
+++ src/lib/plugins/files-option.js
@@ -2,18 +2,20 @@
 
 import { inlineGrammer } from '../utils'
 
-export function use ( config, options ) {
-  if ( Array.isArray(options.files) && options.files.length ) {
+export function use ( config ) {
+  if ( !config.passes.preprocess ) {
+    config.passes = Object.assign({ preprocess: [] }, config.passes)
+  }
 
-    if ( !config.passes.preprocess ) {
-      config.passes = Object.assign({ preprocess: [] }, config.passes)
-    }
+  config.passes.preprocess.push(( ast, options ) => {
+    const files = options.files
 
-    config.passes.preprocess.push(( ast, options ) => {
-      options.files.forEach(filename => {
+    if ( Array.isArray(files) && files.length ) {
+
+      files.forEach(filename => {
         inlineGrammer(filename, ast, config.parser)
       })
-    })
 
-  }
+    }
+  })
 }
